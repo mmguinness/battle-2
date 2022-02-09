3720953954489eb1ca91ae1ecc3b3fea953a9ecf
Marie Mc Guinness <mcguinnessmarie@gmail.com>
2022-02-09 17:11:55 +0000

    Player points displayed

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -17,3 +17,14 @@ describe "Enter player names", type: :feature do
         expect(page).to have_content('Player One: Marie vs Player Two: Tim')
     end
 end
+
+describe "show hit point", type: :feature do
+    it "should allow player one to see player two's hits" do
+        visit '/'
+        fill_in 'player_one', with: 'Marie'
+        fill_in 'player_two', with: 'Tim'
+        click_on 'Enter' 
+        expect(page).to have_content('Tim Hit Points: 100')
+        expect(page).to have_content('Marie Hit Points: 100')
+    end
+end
