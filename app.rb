95d86c84f7fd1c62f84b1d4ad62857314ef879db
Marie Mc Guinness <mcguinnessmarie@gmail.com>
2022-02-09 16:37:52 +0000

    First feature test passing

--- app.rb
+++ app.rb
@@ -7,7 +7,13 @@ class Battle < Sinatra::Base
     end
 
     get '/' do
-        'Testing infrastructure working!'
+        erb(:index)
+    end
+
+    post '/names' do
+        @player_one = params[:player_one]   
+        @player_two = params[:player_two]   
+        erb(:play)  
     end
 
     run! if app_file == $0
--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -1,9 +1,19 @@
 require './app.rb'
 require 'capybara/rspec'
 
-describe "Testing infrastructure", type: :feature do
-    it "should show 'Testing infrastructure working!'" do
+# describe "Testing infrastructure", type: :feature do
+#     it "should show 'Testing infrastructure working!'" do
+#         visit '/'
+#         expect(page).to have_content 'Testing infrastructure working!'
+#     end
+# end
+
+describe "Enter player names", type: :feature do
+    it "should allow players to enter names and see them on srceen" do
         visit '/'
-        expect(page).to have_content 'Testing infrastructure working!'
+        fill_in 'player_one', with: 'Marie'
+        fill_in 'player_two', with: 'Tim'
+        click_on 'Enter' 
+        expect(page).to have_content('Player One: Marie vs Player Two: Tim')
     end
 end
