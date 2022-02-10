3b9aa46a28bc6beccfa4da00a2d5c0097965b9fe
Francesco <francesco@monzo.com>
2022-02-10 19:05:28 +0100

    added test to accept two player instances in game

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,16 @@
 require './lib/player.rb'
 
 class Game
+  attr_reader :players
+
+  def initialize
+    @players = []
+  end
+  
+  def add_players(player_one, player_two)
+    @players << player_one
+    @players << player_two
+  end
 
   def attack(player)
     player.hit_points -= 10
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,6 +2,14 @@ require './lib/game.rb'
 require './lib/player.rb'
 
 describe Game do
+  it "should accept two players" do 
+    game = Game.new
+    tom = Player.new("tom")
+    jerry = Player.new("jerry")
+    game.add_players(tom, jerry)
+    expect(game.players.size).to eq 2
+  end
+
   describe "#attack" do 
     it "should reduce the opponent's points by 10" do 
       game = Game.new
@@ -9,5 +17,5 @@ describe Game do
       game.attack(player) 
       expect(player.hit_points).to eq 90
     end 
-  end 
+  end
 end
